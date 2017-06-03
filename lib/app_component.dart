abc8d8dbce838805367b569762aa85c37befb8f0
mjeffw <mjeffw@bellsouth.net>
2017-06-03 15:31:21 -0700

    check

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -16,15 +16,36 @@ import 'package:angular_components/angular_components.dart';
 )
 class AppComponent {
   static List<String> effects = ['Sense', 'Strengthen', 'Restore', 'Control', 'Destroy', 'Create', 'Transform'];
+  SelectionOptions<String> effectList = new SelectionOptions.fromList(effects);
+  SelectionModel<String> effectSelectModel = new SelectionModel.withList(selectedValues: [effects[0]]);
+  String get singleSelectEffectLabel =>
+      effectSelectModel.selectedValues.length > 0 ? effectSelectModel.selectedValues.first : 'Select Language';
+  String get singleSelectedEffect =>
+      effectSelectModel.selectedValues.isNotEmpty ? effectSelectModel.selectedValues.first : null;
+
+  static List<String> paths = [
+    'Arcanum',
+    'Augury',
+    'Demonology',
+    'Elementalism',
+    'Mesmerism',
+    'Necromancy',
+    'Protection',
+    'Transfiguration'
+  ];
+  SelectionOptions<String> pathList = new SelectionOptions.fromList(paths);
+  SelectionModel<String> pathSelectModel = new SelectionModel.withList(selectedValues: [paths[0]]);
+  String get singleSelectPathLabel =>
+      pathSelectModel.selectedValues.length > 0 ? pathSelectModel.selectedValues.first : 'Select Language';
+  String get singleSelectedPath =>
+      pathSelectModel.selectedValues.isNotEmpty ? pathSelectModel.selectedValues.first : null;
 
   String name = 'Alarm';
   List<String> strings = ['foo', 'bar', 'baz'];
-  SelectionOptions<String> effectList = new SelectionOptions.fromList(effects);
-  SelectionModel<String> effectSelectModel = new SelectionModel.withList(selectedValues: [effects[0]]);
 
-  String get singleSelectEffectLabel =>
-      effectSelectModel.selectedValues.length > 0 ? effectSelectModel.selectedValues.first : 'Select Language';
-
-  String get singleSelectedEffect =>
-      effectSelectModel.selectedValues.isNotEmpty ? effectSelectModel.selectedValues.first : null;
+  List<Map<String, String>> modifiers = [
+    {'name': 'Altered Traits', 'descr': 'Damage Resistance 6 (Hardened 2, +40%; Tough Skin, -40%) (30)'},
+    {'name': 'Duration', 'descr': '12 minutes (6)'},
+    {'name': 'Subject Weight', 'descr': '1000 lbs. (4)'}
+  ];
 }
