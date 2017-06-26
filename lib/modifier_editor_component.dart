c52796fe540d99747612a49b0d82c9d11ecb4493
mjeffw <mjeffw@bellsouth.net>
2017-06-26 19:33:57 -0400

    modifier_editor in progress

---
+++ lib/modifier_editor_component.dart
@@ -0,0 +1,59 @@
+import 'package:angular_components/angular_components.dart';
+import 'package:angular2/angular2.dart';
+import 'package:gurps_incantation_magic_model/incantation_magic.dart';
+import 'package:gurps_incantation_magic_model/units/gurps_duration.dart';
+
+@Component(
+  selector: 'simple-modifier-editor',
+  styleUrls: const ['app_component.css'],
+  directives: const [COMMON_DIRECTIVES, materialDirectives], // ignore: strong_mode_implicit_dynamic_list_literal
+  templateUrl: 'modifier_editor_component.html',
+  providers: const [materialProviders], // ignore: strong_mode_implicit_dynamic_list_literal
+)
+class ModifierEditorComponent {
+  @Input()
+  Spell spell;
+
+  @Input()
+  int index;
+
+  @Input()
+  Function removeModifierFn;
+
+  RitualModifier _modifier;
+  ModifierDetail detail = new WebModifierDetail();
+
+  @Input()
+  set modifier(RitualModifier mod) {
+    _modifier = mod;
+    _modifier.exportDetail(detail);
+  }
+
+  void removeModifier() {
+    spell.ritualModifiers.removeAt(index);
+  }
+
+  void incrementValue() {
+    // TODO _modifier.spellPoints++;
+  }
+}
+
+class WebModifierDetail extends ModifierDetail {
+  String type = 'Simple';
+  String name;
+  int spellPoints;
+  bool inherent;
+  GurpsDuration _duration;
+
+  // TODO: implement summaryText
+  @override
+  String get summaryText => null;
+
+  @override
+  String get typicalText => _duration.toFormattedString();
+
+  @override
+  set value(int value) {
+    _duration = new GurpsDuration(seconds: value);
+  }
+}
