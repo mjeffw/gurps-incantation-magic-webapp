d3d0e91fe438c01bbd3b87e175198f1e518fe982
mjeffw <mjeffw@bellsouth.net>
2018-07-10 08:57:40 -0400

    Altered Traits now require a 'trait' to be defined;

--- lib/info_popup.dart
+++ lib/info_popup.dart
@@ -28,25 +28,26 @@ import 'package:angular_components/angular_components.dart';
           <h3>About the Dungeon Fantasy Incanter's Aid</h3>
         </div>
         
-        <div style="display:table;"> <!-- <div class='t'> -->
-          <div style="display:table-cell;vertical-align:middle;"> <!-- <div class='imgcontainer'> -->
-            <img src="apple-icon-72x72.png">
+        <div>
+          <div style="display:table;"> <!-- <div class='t'> -->
+            <div style="display:table-cell;vertical-align:middle;"> <!-- <div class='imgcontainer'> -->
+              <img src="apple-icon-60x60.png">
+            </div>
+            <p style="padding-left: 5px">
+            Players of <i>GURPS Dungeon Fantasy</i> or the <i>Dungeon Fantasy Roleplaying Game</i> may use this aid to create
+            and calculate the spell point cost of incantations built using the optional Incantation Magic system, described in <a href="http://www.sjgames.com/gurps/books/dungeonfantasy/dungeonfantasy19/">Dungeon Fantasy
+            19 Incantation Magic</a>. A copy of that PDF is required. 
+            </p>
           </div>
-          <p style="padding-left: 5px">
-          Players of <i>GURPS Dungeon Fantasy</i> or the <i>Dungeon Fantasy Roleplaying Game</i> may use this aid to create
-          and calculate the spell point cost of incantations built using the optional Incantation Magic system, described in <a href="http://www.sjgames.com/gurps/books/dungeonfantasy/dungeonfantasy19/">Dungeon Fantasy
-          19 Incantation Magic</a>. A copy of that PDF is required. 
+          
+          <p style='font-size: smaller;'>
+          <i>GURPS</i> and <i>Dungeon Fantasy Roleplaying Game</i> are trademarks of Steve Jackson Games, and its rules 
+          and art are copyrighted by Steve Jackson Games. All rights are reserved by Steve Jackson Games. This game aid 
+          is the original creation of M. Jeff Wilson and is released for free distribution, and not for resale, under the 
+          permissions granted in the <a href="http://www.sjgames.com/general/online_policy.html">Steve Jackson Games 
+          Online Policy</a>.
           </p>
         </div>
-        
-        <p style='font-size: smaller;'>
-        <i>GURPS</i> and <i>Dungeon Fantasy Roleplaying Game</i> are trademarks of Steve Jackson Games, and its rules 
-        and art are copyrighted by Steve Jackson Games. All rights are reserved by Steve Jackson Games. This game aid 
-        is the original creation of M. Jeff Wilson and is released for free distribution, and not for resale, under the 
-        permissions granted in the <a href="http://www.sjgames.com/general/online_policy.html">Steve Jackson Games 
-        Online Policy</a>.
-        </p>
-    
       </material-dialog>
     </modal>  
 ''',
